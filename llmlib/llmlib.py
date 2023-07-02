2d8f59d39a3ec7104379d8a37c94b718c6c29ec3
Mark Watson <markw@markwatson.com>
2023-07-02 08:45:42 -0700

    top level wrapper

--- llmlib/llmlib.py
+++ llmlib/llmlib.py
@@ -1,3 +1,24 @@
 # Main entry for llmlib
 
 import os
+
+# Default to OpenAI API
+
+API_TYPES = ["openai", "fastchat", "huggingface"]
+API_TYPE = os.getenv("LLMLIB_API_TYPE", "openai")
+
+if API_TYPE == "openai":
+    key = os.getenv("OPENAI_API_KEY")
+    if key is None:
+        raise Exception("OPENAI_API_KEY environment variable not set")
+    from llmlib import openai
+    openai.api_key = key
+elif API_TYPE == "fastchat":
+    from llmlib import fastchat
+    pass
+elif API_TYPE == "huggingface":
+    from llmlib import huggingface
+    pass
+else:
+    raise Exception(f"Unknown API type: {API_TYPE}")
+
